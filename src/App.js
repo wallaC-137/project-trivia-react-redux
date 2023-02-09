edb08b0b35091037fa32b1e7b4f2216322917712
Leonardo Dornelles <leonardodornellesfernandes@gmail.com>
2023-02-09 17:55:12 -0300

    Requisito 03 - branch fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
 import Login from './pages/Login';
+import Settings from './pages/settings';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <Login />
+
+        <Switch>
+          <Route component={ Login } path="/" exact />
+          <Route component={ Settings } path="/settings" />
+        </Switch>
+
       </header>
     </div>
   );
