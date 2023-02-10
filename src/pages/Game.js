daab4bd76b5101342116f797632133ab139f58f2
Leonardo Dornelles <leonardodornellesfernandes@gmail.com>
2023-02-10 11:59:54 -0300

    Requisito 04 - Lint fix

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Game extends React.Component {
   render() {
     return (
-      <h1 data-testid="inclusao"> Game </h1>
+      <div>
+        <h1> Game </h1>
+      </div>
     );
   }
 }
