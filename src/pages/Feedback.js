ed3ea01a1c857e95b125d3a3292732a20d4ec814
joao-pedro <joao-pedroassis@hotmail.com>
2023-02-13 16:08:24 -0300

    req15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Feedback extends Component {
+  clickButton = async () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <div>
+      <div className="feedback">
         <h1 data-testid="feedback-text">Feedback</h1>
+        <button
+          type="submit"
+          onClick={ this.clickButton }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Feedback;
