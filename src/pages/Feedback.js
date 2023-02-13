aaf82237ed76ba473a214fa967479014b2bd641c
biel-igl <gabrielpires524@gmail.com>
2023-02-13 17:32:29 -0300

    fa requisito 14

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,22 +4,32 @@ import { connect } from 'react-redux';
 
 class Feedback extends Component {
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     const number = 3;
     return (assertions >= number ? (
       <div>
         <h1 data-testid="feedback-text">Well Done!</h1>
-      </div>) : (<div><h1 data-testid="feedback-text">Could be better...</h1></div>)
+        <p data-testid="feedback-total-question">{assertions}</p>
+        <p data-testid="feedback-total-score">{score}</p>
+      </div>)
+      : (
+        <div>
+          <h1 data-testid="feedback-text">Could be better...</h1>
+          <p data-testid="feedback-total-question">{assertions}</p>
+          <p data-testid="feedback-total-score">{score}</p>
+        </div>)
     );
   }
 }
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 Feedback.propTypes = {
   assertions: PropTypes.string.isRequired,
+  score: PropTypes.string.isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
